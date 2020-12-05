64dd08d552000af002d77baa889805a3c530d653
Leonhard Riedisser <leo008180@googlemail.com>
2020-12-05 11:45:34 +0100

    Day 1

--- aoc2020/src/Day01.hs
+++ aoc2020/src/Day01.hs
@@ -1,2 +1,22 @@
 module Day01 where
 
+import Data.List.Split
+import Relude
+
+example :: [Int]
+example = [1721, 979, 366, 299, 675, 1456]
+
+-- this solution will be incorrect if the input contains 1010
+evaluate1 :: [Integer] -> Maybe Integer
+evaluate1 xs = listToMaybe [x * y | x <- xs, y <- xs, x + y == 2020]
+
+evaluate2 :: [Integer] -> Maybe Integer
+evaluate2 xs = listToMaybe [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
+
+run :: IO ()
+run = do
+  inputRaw <- splitOn "\n" <$> readFile "inputs/day1.txt"
+  let input = catMaybes $ readMaybe <$> inputRaw
+  case evaluate2 input of
+    Nothing -> print ("No result found" :: String)
+    Just i -> print i
