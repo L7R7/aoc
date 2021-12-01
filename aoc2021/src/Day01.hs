6bdb81d5e9926315931534b7e57728767d2b583d
Leonhard Riedisser <leo008180@googlemail.com>
2021-12-01 10:43:59 +0100

    Update solution for day 1
    It doesn't matter that much, but these solutions only need one iteration through the list of inputs

--- aoc2021/src/Day01.hs
+++ aoc2021/src/Day01.hs
@@ -3,7 +3,7 @@
 
 module Day01 where
 
-import Data.List (zipWith3)
+import Data.List (zipWith4)
 import Data.List.Split
 import Relude
 
@@ -11,26 +11,34 @@ example :: [Int]
 example = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
 evaluate1 :: [Int] -> Int
-evaluate1 xs = getSum $ foldMap f (zip xs (drop 1 xs))
+evaluate1 xs = getSum $ fold $ zipWith f xs (drop 1 xs)
 
-f :: (Ord a, Num b) => (a, a) -> Sum b
-f (a, b)
+f :: (Ord a, Num b) => a -> a -> Sum b
+f a b
   | b > a = Sum 1
   | otherwise = Sum 0
 
 evaluate2 :: [Int] -> Int
-evaluate2 xs = evaluate1 $ zipWith3 (\a b c -> a + b + c) xs (drop 1 xs) (drop 2 xs)
+evaluate2 xs = getSum $ fold $ zipWith4 g xs (drop 1 xs) (drop 2 xs) (drop 3 xs)
+
+g :: Int -> Int -> Int -> Int -> Sum Int
+g a b c d = if x2 > x1 then Sum 1 else Sum 0
+  where
+    x1 = a + b + c
+    x2 = b + c + d
 
 run :: IO ()
 run = do
   inputRaw <- splitOn "\n" <$> readFile "inputs/day01.txt"
   let input = catMaybes $ readMaybe <$> inputRaw
+
   putStrLn "=== Part 1"
   let solution1 = evaluate1 example
   putStrLn $ "Solution is correct for example input: " <> show (solution1 == 7)
   let solution2 = evaluate1 input
   print solution2
   putStrLn $ "Solution is correct for input: " <> show (solution2 == 1791)
+
   putStrLn "=== Part 2"
   let solution3 = evaluate2 example
   putStrLn $ "Solution is correct for example input: " <> show (solution3 == 5)
