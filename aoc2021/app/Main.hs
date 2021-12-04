bbce13ddeb89b6f55f2f11474709d114895bd56c
Leonhard Riedisser <leo008180@googlemail.com>
2021-12-04 18:13:27 +0100

    day 4

--- aoc2021/app/Main.hs
+++ aoc2021/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Day03
+import Day04
 import Relude (IO)
 
 main :: IO ()
---
+++ aoc2021/src/Day04.hs
@@ -0,0 +1,117 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Day04 where
+
+import qualified Control.Monad.Combinators.NonEmpty as NE
+import Relude hiding (init)
+import Relude.Extra (fmapToSnd)
+import Text.Megaparsec
+import qualified Text.Megaparsec as T
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer (decimal)
+
+exampleParsed :: Input
+exampleParsed =
+  Input
+    (7 :| [4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1])
+    [ [ [22, 13, 17, 11, 0],
+        [8, 2, 23, 4, 24],
+        [21, 9, 14, 16, 7],
+        [6, 10, 3, 18, 5],
+        [1, 12, 20, 15, 19]
+      ],
+      [ [3, 15, 0, 2, 22],
+        [9, 18, 13, 17, 5],
+        [19, 8, 7, 25, 23],
+        [20, 11, 10, 24, 4],
+        [14, 21, 16, 12, 6]
+      ],
+      [ [14, 21, 17, 24, 4],
+        [10, 16, 15, 9, 19],
+        [18, 8, 23, 26, 20],
+        [22, 11, 13, 6, 5],
+        [2, 0, 12, 3, 7]
+      ]
+    ]
+
+parseInput :: Parsec Void Text Input
+parseInput = Input <$> numbersParser <* skipCount 2 newline <*> boardsParser
+  where
+    numbersParser = decimal `NE.sepBy1` char ','
+    boardLineParser = someTill (skipMany (char ' ') >> decimal) newline
+    boardParser = T.some boardLineParser
+    boardsParser = boardParser `sepBy` newline
+
+type Board = [[Int]]
+
+data Input = Input
+  { numbers :: NonEmpty Int,
+    boards :: [Board]
+  }
+  deriving (Eq, Show)
+
+evaluate1 :: Input -> Maybe Int
+evaluate1 (Input (n :| nums) bs) = calculateScore <$> (extractWinningCombination =<< findWinningStep)
+  where
+    findWinningStep :: Maybe Step
+    findWinningStep = find wins (scanl' (<>) (init bs n) (init bs <$> nums))
+    extractWinningCombination :: Step -> Maybe (Int, BoardState)
+    extractWinningCombination (Step (Last num) states) = do
+      i <- num
+      winning <- find wins' states
+      pure (i, winning)
+    calculateScore :: (Int, BoardState) -> Int
+    calculateScore (i, BoardState states) = i * sum (fst <$> filter ((== False) . snd) (join states))
+
+newtype BoardState = BoardState [[(Int, Bool)]] deriving newtype (Show)
+
+instance Semigroup BoardState where
+  (BoardState bs) <> (BoardState bs') = BoardState $ zipWith (zipWith (\(_, b) (i', b') -> (i', b || b'))) bs bs'
+
+data Step = Step
+  { number :: Last Int,
+    boardsState :: [BoardState]
+  }
+  deriving (Show)
+
+instance Semigroup Step where
+  (Step n bs) <> (Step n' bs') = Step (n <> n') (zipWith (<>) bs bs')
+
+wins :: Step -> Bool
+wins (Step _ bs) = any wins' bs
+
+wins' :: BoardState -> Bool
+wins' (BoardState bs) = lineWins bs || lineWins (transpose bs)
+  where
+    lineWins xs = or (fmap (all ((== True) . snd)) xs)
+
+init :: [Board] -> Int -> Step
+init bs i = Step (Last (Just i)) (BoardState . fmap (fmapToSnd (== i)) <$> bs)
+
+evaluate2 :: _
+evaluate2 inputs = undefined
+
+run :: IO ()
+run = do
+  example <- readFileText "inputs/day04-example.txt"
+  putStrLn $ "Parsing the example works: " <> show (parseMaybe parseInput example == Just exampleParsed)
+  inputRaw <- readFileText "inputs/day04.txt"
+
+  case parseMaybe parseInput inputRaw of
+    Nothing -> putStrLn "Parsing the inputs failed"
+    Just input -> do
+      putStrLn "=== Part 1"
+      let solution1 = evaluate1 exampleParsed
+      putStrLn $ "Solution for example is: " <> show solution1
+      putStrLn $ "Solution is correct for example input: " <> show (solution1 == Just 4512)
+      let solution2 = evaluate1 input
+      putStrLn $ "Solution for input is: " <> show solution2
+      putStrLn $ "Solution is correct for input: " <> show (solution2 == Just 44736)
+
+-- putStrLn "=== Part 2"
+-- let solution3 = evaluate2 exampleParsed
+-- putStrLn $ "Solution is correct for example input: " <> show (solution3 == 1924)
+-- let solution4 = evaluate2 input
+-- putStrLn $ "Solution for input is: " <> show solution4
+-- putStrLn $ "Solution is correct for input: " <> show (solution4 == 3379326)
