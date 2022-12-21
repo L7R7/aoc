7d739582b6b45fac6f8a481360458b3e1d4a7cf1
Leonhard Riedisser <leo008180@googlemail.com>
2022-12-21 12:28:05 +0100

    day 08

--- aoc2022/src/Day08.hs
+++ aoc2022/src/Day08.hs
@@ -1,8 +1,16 @@
 module Day08 (run) where
 
 import Data.Char (digitToInt)
-import Data.List (zipWith4)
+import Data.List (maximum, zipWith4)
+import qualified Data.Map as M
+import Math.Geometry.Grid
+import Math.Geometry.Grid.Square
+import Math.Geometry.Grid.SquareInternal (SquareDirection (..))
+import Math.Geometry.GridMap ((!))
+import qualified Math.Geometry.GridMap as G
+import Math.Geometry.GridMap.Lazy
 import Relude hiding (many)
+import qualified Relude.Unsafe as Unsafe
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
@@ -31,6 +39,30 @@ evaluate1 leftToRight = foldMap (\b -> if b then Sum 1 else mempty) resGrid
     g Nothing n = (Just n, True)
     g (Just prev) n = (Just (max prev n), prev < n)
 
+type Input2 = LGridMap RectSquareGrid Int
+
+createGrid :: [[Int]] -> Input2
+createGrid ts = lazyGridMap (rectSquareGrid rows columns) (join ts)
+  where
+    columns = length ts
+    rows = length $ Unsafe.head ts
+
+evaluate2 :: Input1 -> Int
+evaluate2 = evaluate2' . createGrid
+
+evaluate2' :: Input2 -> Int
+evaluate2' grid = maximum $ snd <$> M.toList combinedDistances
+  where
+    combinedDistances = foldl' (\acc g -> M.unionWith (*) acc (G.toMap g)) M.empty distances
+    distances = (\direction -> G.mapWithKey (f direction) grid) <$> [North, South, East, West]
+    f :: SquareDirection -> (Int, Int) -> Int -> Int
+    f direction index value = go index
+      where
+        go :: (Int, Int) -> Int
+        go ix = case neighbour grid ix direction of
+          Nothing -> 0
+          Just in' -> 1 + if value > grid ! in' then go in' else 0
+
 run :: IO ()
 run = do
   exampleInputRaw <- readFileText "inputs/08-example.txt"
@@ -40,3 +72,6 @@ run = do
   inputRaw <- readFileText "inputs/08.txt"
   let parsed1 = parseMaybe inputParser inputRaw
   print $ traverse evaluate1 parsed1
+
+  print $ evaluate2 <$> exampleParsed1
+  print $ evaluate2 <$> parsed1
